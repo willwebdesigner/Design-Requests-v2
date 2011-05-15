abdfb34a4fdbf55d063b05a2a11a0eb973c4e41c
William Li <willi287@hotmail.com>
2011-05-15 14:49:25 +0100

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RequestForm::Application.routes.draw do
   root :to => 'pages#home'  
   match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
 
   # The priority is based upon order of creation:
