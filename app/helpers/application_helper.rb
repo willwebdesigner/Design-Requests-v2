141e08e5bd349c863ab5e4562e1e014694a87309
William Li <willi287@hotmail.com>
2011-05-15 14:33:57 +0100

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Design Request Form"
+    if @title.nil? #Could also be spelt as @title == 0
+      base_title
+    else
+      "#{base_title} | #{@title}" 
+    end
+  end
+  
 end
